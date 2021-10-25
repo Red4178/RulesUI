a22286219d99e6f97e73f999cef25c8678f8464e
Red4178 <93115595+Red4178@users.noreply.github.com>
2021-10-25 10:10:35 +0300

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,3 +1,11 @@
 <php?
       
 namespace Red4178;
+      
+use pocketmine\plugin\PluginBase;
+use pocketmine\Command;
+use pocketmine\command\CommandSender;
+      
+use pocketmine\utils\TextFormat as c;
+
+class Main extends PluginBase
